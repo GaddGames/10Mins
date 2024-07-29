585f68ec53d0bd1be45f7d8c785629651d4ded19
Laurence Gadd <115166987+GaddGames@users.noreply.github.com>
2024-07-29 22:53:45 +0100

    Commit Build

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "Gadd Games-10 Minutes To Midnight-0.1.0";
+const contentToCache = [
+    "Build/10MinsB.loader.js",
+    "Build/10MinsB.framework.js.br",
+    "Build/10MinsB.data.br",
+    "Build/10MinsB.wasm.br",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
